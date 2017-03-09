a95dd17889b83b644ddd42875f05d8d3222d94db
Frank Nelles <karlo261@hotmail.de>
2017-03-09 20:28:56 +0100

    Rechanged main form on program start
    Signed-off-by: Frank Nelles <karlo261@hotmail.de>

--- Stenographie_Trainingtool/Program.cs
+++ Stenographie_Trainingtool/Program.cs
@@ -16,7 +16,7 @@ namespace Stenographie_Trainingtool
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new formICan());
+            Application.Run(new formStenoTrainerMain());
         }
     }
 }
